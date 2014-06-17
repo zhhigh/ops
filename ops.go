8ec4726c4469342e7492180d67c6bdbbb9c51894
zhhigh <hdbmaster@139>
2014-06-17 16:29:06 +0800

    v1.0

---
+++ ops.go
@@ -0,0 +1,22 @@
+// Copyright 2013  <zhigh@163.com> All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+This module is Gearman API for golang. 
+The protocol was implemented by native way.
+*/
+
+package	ops 
+
+import (
+    _ "ops/mongo"
+    _ "ops/ssh2"
+    _ "ops/algorithm"
+	_ "ops/version"
+	_ "ops/business"
+	_ "ops/opsworker"
+	_ "ops/conf"
+	_ "ops/logs"
+)
+
